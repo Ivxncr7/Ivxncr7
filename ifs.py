02002d003f007a31447405db25c6b228a35bebc8
Ivan Lopez <edwinivanlopezaleman89>
2024-10-07 10:32:15 -0500

    es todo lo que pude perdonn

---
+++ ifs.py
@@ -0,0 +1,18 @@
+def printLetterGrade():
+    grade = 95#use int(input()) here
+    if grade >= 95:
+        print("A+")
+    if grade < 95 and grade >= 92:
+        print("A")
+            # Add more if statements here
+    if grade >=92:
+       print("B+")
+    if grade < 85 and grade >= 82:
+       print("B+")
+
+    
+    
+if __name__ =="__main__":
+    printLetterGrade()
+
+
