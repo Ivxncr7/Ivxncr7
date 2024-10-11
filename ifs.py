ad4f56178691707aaeb9ac90e419275426773974
Ivan Lopez <edwinivanlopezaleman89>
2024-10-11 10:31:50 -0500

    Jugador 1 gana

--- ifs.py
+++ ifs.py
@@ -1,18 +1,29 @@
 def printLetterGrade():
-    grade = 95#use int(input()) here
-    if grade >= 95:
-        print("A+")
-    if grade < 95 and grade >= 92:
+    num= int(input())
+    if num >=90 and num <= 100:
         print("A")
-            # Add more if statements here
-    if grade >=92:
-       print("B+")
-    if grade < 85 and grade >= 82:
-       print("B+")
+    if num >=80 and num<=90:
+        print("B")
 
-    
-    
+    if num >=70 and num<=80:
+        print("C")
+
+    if num >=55 and num<=70:
+        print("D")
+
+    if num >=50 and num<=55:
+        print("E")
+
+def rps():
+    choice1 = input()
+    choice2 = input()
+
+    if  choice1== "scissors" and choice2 =="paper":
+        print ("player1 wins")
+
+
 if __name__ =="__main__":
     printLetterGrade()
+    rps()
 
 
